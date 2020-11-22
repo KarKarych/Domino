36f9c03b629823f131e735511470c4b28964a3b1
Иван Сметанин <smetaninivanu@gmail.com>
2020-11-22 22:32:02 +0300

    Rename class

--- src/test/java/ru/vsu/dominoes/db/DataStorageTest.java
+++ src/test/java/ru/vsu/dominoes/db/DataStorageTest.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class DataBaseDataStorageTest {
+public class DataStorageTest {
   DataStorage dataStorage;
 
   @BeforeEach
